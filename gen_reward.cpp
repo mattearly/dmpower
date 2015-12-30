55c34e4b7102a37d275a5145e32bd35118bb2aa8
Matthew <matthewjearly@gmail.com>
2015-12-30 17:17:19 -0500

    added cr list notification to user

--- gen_reward.cpp
+++ gen_reward.cpp
@@ -4520,8 +4520,9 @@ string Magic_Items::GenerateArt(const int& amount, const int& value) const {
 float Magic_Items::xpgenerator() {
     float xp = 0;
     int cr = 0;
+	vector<float> crlist;
     char ans;
-    cout << "\nA Reference Level Chart (FYI):\n\n"
+    cout << "A Reference Level Chart (FYI):\n\n"
          << " 2. 300\n"
          << " 3. 900\n"
          << " 4. 2,700\n"
@@ -4561,110 +4562,149 @@ float Magic_Items::xpgenerator() {
              << "15. CR 11     32. CR 28\n"
              << "16. CR 12     33. CR 29\n"
              << "17. CR 13     34. CR 30\n\n"
-             << " tip: add 'y' after your choice to continue picking.(ex: 9y)\n\n";
-        cr = getNumber("Choose CR by the chart(1-34)", 1, 34);
+             << " tip: add 'y' after your choice to continue picking.(ex: 9y)\n"
+			 << "CRs so far: ";
+        for (auto i : crlist) {
+			cout << i << ", ";
+        }
+		cout << endl;
+		cr = getNumber("Choose CR by the chart(1-34): ", 1, 34);
         switch (cr) {
         case 1:
             xp += 10;
+            crlist.push_back(0);
             break;
         case 2:
             xp += 25;
+            crlist.push_back(.125);
             break;
         case 3:
             xp += 50;
+            crlist.push_back(.25);
             break;
         case 4:
             xp += 100;
+			crlist.push_back(.50);
             break;
         case 5:
             xp += 200;
+			crlist.push_back(1);
             break;
         case 6:
             xp += 450;
+			crlist.push_back(2);
             break;
         case 7:
             xp += 700;
+			crlist.push_back(3);
             break;
         case 8:
             xp += 1100;
+			crlist.push_back(4);
             break;
         case 9:
             xp += 1800;
+			crlist.push_back(5);
             break;
         case 10:
             xp += 2300;
+			crlist.push_back(6);
             break;
         case 11:
             xp += 2900;
+			crlist.push_back(7);
             break;
         case 12:
             xp += 3900;
+			crlist.push_back(8);
             break;
         case 13:
             xp += 5000;
+			crlist.push_back(9);
             break;
         case 14:
             xp += 5900;
+			crlist.push_back(10);
             break;
         case 15:
             xp += 7200;
+			crlist.push_back(11);
             break;
         case 16:
             xp += 8400;
+			crlist.push_back(12);
             break;
         case 17:
             xp += 10000;
+			crlist.push_back(13);
             break;
         case 18:
             xp += 11500;
+			crlist.push_back(14);
             break;
         case 19:
             xp += 13000;
+			crlist.push_back(15);
             break;
         case 20:
             xp += 15000;
+			crlist.push_back(16);
             break;
         case 21:
             xp += 18000;
+			crlist.push_back(17);
             break;
         case 22:
             xp += 20000;
+			crlist.push_back(18);
             break;
         case 23:
             xp += 22000;
+			crlist.push_back(19);
             break;
         case 24:
             xp += 25000;
+			crlist.push_back(20);
             break;
         case 25:
             xp += 33000;
+			crlist.push_back(21);
             break;
         case 26:
             xp += 41000;
+			crlist.push_back(22);
             break;
         case 27:
             xp += 50000;
+			crlist.push_back(23);
             break;
         case 28:
             xp += 62000;
+			crlist.push_back(24);
             break;
         case 29:
             xp += 75000;
+			crlist.push_back(25);
             break;
         case 30:
             xp += 90000;
+			crlist.push_back(26);
             break;
         case 31:
             xp += 105000;
+			crlist.push_back(27);
             break;
         case 32:
             xp += 120000;
+			crlist.push_back(28);
             break;
         case 33:
             xp += 135000;
+			crlist.push_back(29);
             break;
         case 34:
             xp += 155000;
+			crlist.push_back(30);
         default:break;
         }
         cout << "Total unsplit so far: " << xp << "xp" << endl;
