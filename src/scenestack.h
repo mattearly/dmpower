1f4180b2020beafe40e878f2af9e5d5faa64ea96
Matthew Early <matthewjearly@gmail.com>
2017-02-11 21:33:27 -0500

    working on character menu

--- src/scenestack.h
+++ src/scenestack.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "sdl_graphics.h"
 #include "texture.h"
 
@@ -5,12 +6,8 @@ class SceneStack {
 
 public:
 	SceneStack(){
-//		Leadcoat=TTF_OpenFont("res/fonts/Leadcoat.ttf", 54);
-//		if(!Leadcoat) { printf("TTF_OpenFont Leadcoat: %s\n", TTF_GetError()); }
 	}
 	~SceneStack(){
-//		TTF_CloseFont(Leadcoat);
-//		Leadcoat = NULL;
 	}
 	void mainscreen(SDL_graphics &graphics_engine);
 
@@ -25,14 +22,56 @@ private:
 	void menuScene4_tools(SDL_graphics &graphics_engine);
 	void menuScene5_export(SDL_graphics &graphics_engine);
 
-	int CycleRight_5(const int &, const int &);
+    /// returns 0-4 as appropriate for our mainscreen window menu
+    int CycleRight_5(const int &amt, const int &current){
+        switch (amt) {
+        case 1: {
+            switch (current) {
+            case 0: return 1;
+            case 1: return 2;
+            case 2: return 3;
+            case 3: return 4;
+            case 4: return 0;
+            default:break;
+            } break;
+        }
+        case 2:{
+            switch (current) {
+            case 0: return 2;
+            case 1: return 3;
+            case 2: return 4;
+            case 3: return 0;
+            case 4: return 1;
+            default:break;
+            } break;
+        }
+        case 3:{
+            switch (current) {
+            case 0: return 3;
+            case 1: return 4;
+            case 2: return 0;
+            case 3: return 1;
+            case 4: return 2;
+            default:break;
+            } break;
+        }
+        case 4:{
+            switch (current) {
+            case 0: return 4;
+            case 1: return 0;
+            case 2: return 1;
+            case 3: return 2;
+            case 4: return 3;
+            default:break;
+            } break;
+        }
+        default:break;
+        }
+        return 0;
+    };
 	bool newSceneProcced = false;
 
-	/// PRELOADED COLORS
+	/// PRELOAD COLORS
 	SDL_Color Black = {0,0,0,0};
 	SDL_Color Orange = {255, 115, 35, 0};
-
-	/// TITLE FONT
-//	TTF_Font *Leadcoat;
-
 };
