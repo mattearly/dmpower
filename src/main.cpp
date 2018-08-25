5642105316e1cf63789e79f020d5beb96e93c776
Matthew Early <matthewjearly@gmail.com>
2018-08-25 12:16:14 -0400

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,10 @@
-#include "campaign.h"
-#include <string>
-#include <iostream>
-#include "globalfuncts.h"
-
-extern void main_loop();
-
-using std::string;
+extern void menu();
 
 int main()
 {
-    /* Program Header */
-    std::cout << "|-------------------------------------------------------|" << std::endl;
-    std::cout << "|-----[CLI DUNGEON MASTER TOOLKIT] [v000] [build:1]-----|" << std::endl;
-    std::cout << "|----------your terminal should fit this block----------|" << std::endl;
-    std::cout << "|-----------without wrapping for best results-----------|" << std::endl;
-    std::cout << "|------------------58 character block-------------------|" << std::endl;
-    std::cout << "|-------------------------------------------------------|" << std::endl;
+    menu();
 
-    pressEnterToContinue();
-
-    /* Initiate Main Program Menu */
-    main_loop();
-
-    return EXIT_SUCCESS;
-
+    return 0;
 }
+
+
