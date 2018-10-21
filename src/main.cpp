1f54467e0ae881f48265da8766b6ca911481815b
Matthew Early <matthewjearly@gmail.com>
2018-10-20 23:37:56 -0400

    local user settings load on program startup

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,7 @@ extern void set_user_pref_from_file();
 int main()
 {
 
-
-  //todo
-  //check user settings
-  //if they dont exist, create and set as default
-  //if they do exist, load their values in
-
-  //when settings change, reload them or make sure they are set correctly in general
-
-//  loadUserSettings();
+  set_user_pref_from_file();
 
   menu();
 
