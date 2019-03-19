33620ec206493767727b9d6b79d3526c3651d6e7
Matthew Early <matthewjearly@gmail.com>
2019-03-19 00:27:12 -0400

    commented out unused color strings, removed static (already implied), added header guard

--- src/terminal_colors.h
+++ src/terminal_colors.h
@@ -1,25 +1,23 @@
+#pragma once
 #include <string>
-
-static const std::string BLINK = "\e[5m";
-static const std::string BOLD = "\e[1m";
-static const std::string RESET = "\e[0m";
-static const std::string ERROR = "\e[1;41;37m\a";
-static const std::string MENU = "\e[44;37m";
-
-static const std::string BLACK = "\e[30m";
-static const std::string RED = "\e[31m";
-static const std::string GREEN = "\e[32m";
-static const std::string YELLOW = "\e[33m";
-static const std::string BLUE = "\e[34m";
-static const std::string MAGENTA = "\e[35m";
-static const std::string CYAN = "\e[36m";
-static const std::string WHITE = "\e[37m";
-
-static const std::string B_BLACK = "\e[40m";
-static const std::string B_RED = "\e[41m";
-static const std::string B_GREEN = "\e[42m";
-static const std::string B_YELLOW = "\e[43m";
-static const std::string B_BLUE = "\e[44m";
-static const std::string B_MAGENTA = "\e[45m";
-static const std::string B_CYAN = "\e[46m";
-static const std::string B_WHITE = "\e[47m";
+// const std::string BLINK = "\e[5m";
+// const std::string BOLD = "\e[1m";
+const std::string RESET = "\e[0m";
+// const std::string ERROR = "\e[1;41;37m\a";
+// const std::string MENU = "\e[44;37m";
+// const std::string BLACK = "\e[30m";
+const std::string RED = "\e[31m";
+const std::string GREEN = "\e[32m";
+const std::string YELLOW = "\e[33m";
+const std::string BLUE = "\e[34m";
+const std::string MAGENTA = "\e[35m";
+const std::string CYAN = "\e[36m";
+// const std::string WHITE = "\e[37m";
+// const std::string B_BLACK = "\e[40m";
+// const std::string B_RED = "\e[41m";
+// const std::string B_GREEN = "\e[42m";
+// const std::string B_YELLOW = "\e[43m";
+// const std::string B_BLUE = "\e[44m";
+// const std::string B_MAGENTA = "\e[45m";
+// const std::string B_CYAN = "\e[46m";
+// const std::string B_WHITE = "\e[47m";
